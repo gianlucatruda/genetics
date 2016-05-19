7e8ee28a389f705dd7e2981da8fbbb12804089cd
Gianluca Truda <gianlucatruda@gmail.com>
2016-05-19 13:47:57 +0200

    Updated analyse.py to be functional. It takes user input for filename if none is given in the arguments from terminal call. Updated bash script auto.sh to outline an example of how to perform multiple analyses with a single command.

--- analyse.py
+++ analyse.py
@@ -1,31 +1,46 @@
 # Gianluca Truda
-# 25 Feb 2016
+# Original :	25 Feb 2016
+# Updated :		19 May 2016
 
 import sys
 
-#if sys.argv[0]:	
-name = sys.argv[0]
-#else:
-#	name = input("Enter filename: ")
+# Input control
 
-file = open(name, 'r', encoding = 'utf-8')
+if (len(sys.argv) == 2):
+	if (sys.argv[1] != ""): name = sys.argv[1]
+elif (len(sys.argv)>2):
+	print("Error: too many arguments given!")
+	sys.exit()
+else:
+	name = input("Enter filename: ")
 
-inStr = file.read()
+# Variable initiation
 
 counts = {'A':0,'C':0,'G':0,'T':0}
+file = open(name, 'r', encoding = 'utf-8')
+inStr = file.read()
+
+# Reading from file
 
 for i in range(len(inStr)):
     if inStr[i] == "A" or inStr[i] == "C" or inStr[i] == "G" or inStr[i] == "T":
         counts[inStr[i]] += 1
     
+
+# Output of data
+
 total = counts['A'] + counts['C'] + counts['G'] + counts['T']
 print("\n\n",
-      "C:",counts['C'], ":", round(counts['C']*100/total, 2),"%", "\n",
-      "G:",counts['G'], ":", round(counts['G']*100/total, 2),"%", "\n", 
-      "A:",counts['A'], ":", round(counts['A']*100/total, 2),"%", "\n",
-      "T:",counts['T'], ":", round(counts['T']*100/total, 2),"%", "\n\n", 
+      "C:",counts['C'], ":\t", round(counts['C']*100/total, 2),"%", "\n",
+      "G:",counts['G'], ":\t", round(counts['G']*100/total, 2),"%", "\n", 
+      "A:",counts['A'], ":\t", round(counts['A']*100/total, 2),"%", "\n",
+      "T:",counts['T'], ":\t", round(counts['T']*100/total, 2),"%", "\n\n", 
       "Total:", total)
 
 print("Pyrimidines:\t", counts['C']+ counts['T'])
 print("Purines:\t", counts['A']+ counts['G'])
 print("pyr/pur:\t",round((counts['C']+counts['T'])/(counts['A']+ counts['G']),3)) 
+
+# Shutdown
+sys.exit()
+
--- auto.sh
+++ auto.sh
@@ -1,6 +1,11 @@
-echo Auto Script for Gene Sequence Analysis
+# Gianluca Truda
+# Original : 	19 May 2016
+# Updated : 	19 May 2016
+
+
+echo Auto Script for Gene Sequence Analysis 
+echo 
 echo Chromosome 21:
-python3 analyse.py
-data/s1.txt
+python3 analyse.py 
 
 
