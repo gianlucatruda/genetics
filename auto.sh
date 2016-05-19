1ef1495b567a61343c706c4323b315494c7b625c
Gianluca Truda <trdgia001@myuct.ac.za>
2016-05-19 13:53:59 +0200

    Minor tweaks to auto.sh

--- auto.sh
+++ auto.sh
@@ -2,10 +2,13 @@
 # Original : 	19 May 2016
 # Updated : 	19 May 2016
 
-
+echo
 echo Auto Script for Gene Sequence Analysis 
 echo 
 echo Chromosome 21:
-python3 analyse.py 
+python3 analyse.py data/21.txt 
+echo Chromosome X:
+python3 analyse.py data/x.txt
+echo 
 
 
