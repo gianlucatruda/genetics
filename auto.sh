cdd6b485e86ceedd20d8154cccc5f5eaadfe99a1
Gianluca Truda <gianlucatruda@gmail.com>
2016-05-21 11:46:50 +0200

    Minor readability alterations to auto.sh

--- auto.sh
+++ auto.sh
@@ -1,12 +1,13 @@
 # Gianluca Truda
 # Original : 	19 May 2016
-# Updated : 	19 May 2016
+# Updated : 	21 May 2016
 
 echo
 echo Auto Script for Gene Sequence Analysis 
 echo 
 echo Chromosome 21:
 python3 analyse.py data/21.txt 
+echo
 echo Chromosome X:
 python3 analyse.py data/x.txt
 echo 
